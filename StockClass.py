7887deefa562bf3b2c86818cb96fa9af92bf8263
SamuelDixxon <SamuelDixon@tamu.edu>
2021-07-28 19:46:22 -0500

    Added a print function for the class

--- StockClass.py
+++ StockClass.py
@@ -1,12 +1,18 @@
 class Stock():
 
-    def __init__(self,  ticker, priceCurr, exists):
+    def __init__(self,  ticker, priceCurr, exists, hasDiv):
         # initializing all Stock attributes
         self.ticker = ticker
         self.priceCurr = priceCurr
         self.exists = exists
+        self.hasDiv = hasDiv
 
     # getter methods / setter methods
+
+    def getinfo(self):
+        a = [self.ticker, self.priceCurr, self.exists, self.hasDiv]
+        for i in range(len(a)-1):
+            printf("{}".format(a[i]+"\n"))
 
     def getpriceCurr(self):
         return self.priceCurr
@@ -17,6 +23,9 @@ class Stock():
     def stockexists(self):
         return self.exists
 
+    def divexists(self):
+        return self.hasDiv
+
     def setprice(self, newprice):
         self.priceCurr = newprice
 
@@ -25,3 +34,6 @@ class Stock():
 
     def setexists(self, change):
         self.exists = change
+
+    def sethasdiv(self, change):
+        self.hasDiv = change
