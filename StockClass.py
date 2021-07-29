7351df2f9658c397611b94bfc52e8ba82a664fb6
SamuelDixxon <SamuelDixon@tamu.edu>
2021-07-28 20:04:59 -0500

    Added back getters/setters

--- StockClass.py
+++ StockClass.py
@@ -13,3 +13,27 @@ class Stock():
         a = [self.ticker, self.priceCurr, self.exists, self.hasDiv]
         for i in range(len(a)-1):
             printf("{}".format(a[i]+"\n"))
+
+    def getpriceCurr(self):
+        return self.priceCurr
+
+    def getticker(self):
+        return self.ticker
+
+    def stockexists(self):
+        return self.exists
+
+    def divexists(self):
+        return self.hasDiv
+
+    def setprice(self, newprice):
+        self.priceCurr = newprice
+
+    def setticker(self, newticker):
+        self.ticker = newticker
+
+    def setexists(self, change):
+        self.exists = change
+
+    def sethasdiv(self, change):
+        self.hasDiv = change
