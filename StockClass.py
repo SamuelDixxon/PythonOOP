1e6df31944b09241617a812464d6a57920451f35
SamuelDixxon <SamuelDixon@tamu.edu>
2021-07-28 19:39:42 -0500

    Added attributes, getters, and setters to Stock class

--- StockClass.py
+++ StockClass.py
@@ -1,4 +1,27 @@
-def Stock():
+class Stock():
 
-    def __init__(self):
+    def __init__(self,  ticker, priceCurr, exists):
         # initializing all Stock attributes
+        self.ticker = ticker
+        self.priceCurr = priceCurr
+        self.exists = exists
+
+    # getter methods / setter methods
+
+    def getpriceCurr(self):
+        return self.priceCurr
+
+    def getticker(self):
+        return self.ticker
+
+    def stockexists(self):
+        return self.exists
+
+    def setprice(self, newprice):
+        self.priceCurr = newprice
+
+    def setticker(self, newticker):
+        self.ticker = newticker
+
+    def setexists(self, change):
+        self.exists = change
