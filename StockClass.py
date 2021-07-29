12decff690bc9736c02d25c33fc66caac656853a
SamuelDixxon <SamuelDixon@tamu.edu>
2021-07-28 20:34:00 -0500

    Updated display class with a dictionary

--- StockClass.py
+++ StockClass.py
@@ -1,4 +1,4 @@
-class Stock():
+class Stock:
 
     def __init__(self,  ticker, priceCurr, exists, hasDiv):
         # initializing all Stock attributes
@@ -10,9 +10,10 @@ class Stock():
     # getter methods / setter methods
 
     def getinfo(self):
-        a = [self.ticker, self.priceCurr, self.exists, self.hasDiv]
-        for i in range(len(a)-1):
-            printf("{}".format(a[i]+"\n"))
+        a = {'Ticker: ': self.ticker, 'Current Price: ': self.priceCurr,
+             'Stock Exists': self.exists, 'Has a Dividend: ': self.hasDiv}
+        for key, val in a.items():
+            print("{} {}".format(key, val))
 
     def getpriceCurr(self):
         return self.priceCurr
