613dcf415943e363d293695b02163d6db8c070b0
SamuelDixxon <SamuelDixon@tamu.edu>
2021-07-30 17:03:57 -0500

    Working on configuring anaconda in terminal

--- Test.py
+++ Test.py
@@ -1,5 +1,10 @@
 from StockClass import Stock
+import matplotlib.pyplot as plt
+import numpy as np
 
-abc = Stock('AAPL', '144.98', True, True)
+abc = Stock('AAPL', 144.98, True, True)
 
 abc.getinfo()
+
+plt.plot([1, 2, 3], [15, 5, 6])
+plt.show()
