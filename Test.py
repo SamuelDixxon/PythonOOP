954d21e6a679ce9664c2c3c7a7636a56251d43fc
SamuelDixxon <SamuelDixon@tamu.edu>
2021-07-30 19:59:11 -0500

    Starting to tinker with yfinance package and pandas for data visualization

--- Test.py
+++ Test.py
@@ -1,10 +1,16 @@
 from StockClass import Stock
 import matplotlib.pyplot as plt
 import numpy as np
+import yfinance as yf
 
 abc = Stock('AAPL', 144.98, True, True)
 
-abc.getinfo()
 
-plt.plot([1, 2, 3], [15, 5, 6])
+tickerData = yf.Ticker(abc.ticker)
+
+tickerDf = tickerData.history(period='1m', start='2010-1-1', end='2021-1-1')
+
+
+plt.plot(tickerDf.index, tickerDf['Close'])
+plt.title(abc.ticker + " '2021-1-1 throw 2021-1-25")
 plt.show()
